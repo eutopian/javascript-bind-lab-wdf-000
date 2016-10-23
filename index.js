4aa616d6c9e556fb0046d2316120bd9fe38a647b
Margaret <eutopia@gmail.com>
2016-10-23 17:32:04 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-const app = "I don't do much.";
+// const digitalClock = {
+//   time: Math.round(Date.now() / 1000),
+//   startTicking() {
+//     setInterval(function(){this.time++}.bind(this), 1000);
+//   }
+// };
+
+// const digitalClock = {
+//   time: Math.round(Date.now() / 1000),
+//   startTicking() {
+//   	this.time++
+//     setInterval(() => (this.startTicking).bind(this), 1000);
+//   }
+// };
+
+
+const digitalClock = {
+  time: Math.round(Date.now() / 1000),
+  startTicking: function () {
+    setInterval(() => this.time++, 1000);
+  }
+};
+
+const censor = (cword, word) => {
+	var regex = new RegExp(cword, "g");
+	return word.replace(regex, 'BLEEP');
+}
+
+const violenceCensor = censor.bind("", 'violence');
+const drugsCensor = censor.bind("", 'drugs');
